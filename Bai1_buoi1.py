9ff93e0076ff43f1fea7ba193b8e717912882d53
ngthuyn <71604453+ngthuyn@users.noreply.github.com>
2021-07-24 15:34:30 +0700

    Add files via upload

---
+++ Bai1_buoi1.py
@@ -0,0 +1,12 @@
+#Bài 02. Hãy lập trình đoạn chương trình Python giải quyết các yêu cầu sau
+"""
+1. Yêu cầu người dùng nhập name của họ từ bàn phím.
+2. In ra lời chào với họ đã đến chương trình
+3. Yêu cầu họ nhập năm sinh
+4. Tính toán và in ra số tuổi hiện tại của
+"""
+name=input("Nhập tên của bạn: ")
+print(f"Xin chào {name} đã đến với chương trình")
+nam_sinh=int(input("Nhập năm sinh của bạn: "))
+tuoi=2021-nam_sinh
+print(f'Tuổi của bạn là {tuoi}')
