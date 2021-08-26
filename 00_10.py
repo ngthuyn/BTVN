77d3ef4e3797be69beb574cbe43bf06e5a6310cc
Huong Nguyen <71604453+ngthuyn@users.noreply.github.com>
2021-08-26 19:26:35 +0700

    Update 00_10.py

--- 00_10.py
+++ 00_10.py
@@ -1,10 +1,9 @@
 """Bài 00. Viết một chương trình để lấy ra tên class của một object?"""
+
 class Person:
     def __init__(self,name,age):
         self.name=name
         self.age=age
     
-
 person_1=Person("Hường",20)
-#print(f"{person_1.name} {person_1.age} tuổi")
 print(person_1.__class__)
