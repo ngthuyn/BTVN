8af70b1d81444a705c08b4e8af2210c94fb7f771
Huong Nguyen <71604453+ngthuyn@users.noreply.github.com>
2021-08-30 19:36:28 +0700

    Update 08_12.py

--- 08_12.py
+++ 08_12.py
@@ -2,16 +2,9 @@
 Bài 08: Viết hàm def get_size(file): lấy và trả về dung lượng file
 
 """
+import os
 def get_size(file):
-    f= open(file, mode='r', encoding='utf-8')
-    lines=0
-    Content = f.read()
-    List = Content.split("\n")
-  
-    for i in List:
-        if i:
-            lines += 1
-    return lines
+    print(os.stat(file).st_size)
+   
 
-size=get_size('/Users/Asus/PycharmProjects/pythonCore/f.txt')
-print(size)
+get_size('/Users/Asus/PycharmProjects/pythonCore/A.txt')
